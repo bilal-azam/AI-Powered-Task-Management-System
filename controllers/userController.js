baa7e6bcc0656b2efc07f09bf6271fc1799f90d7
Bilal Azam <bilal.azam1@outlook.com>
2023-06-24 00:00:00 +0100

    Implement user registration endpoint

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = new User({ name, email, password });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+module.exports = { registerUser };
