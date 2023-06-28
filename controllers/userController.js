3c2ac822b2e11792c4143d64a42097e6ec605630
Bilal Azam <bilal.azam1@outlook.com>
2023-06-28 12:00:00 +0100

    Implement user login endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,18 @@
 const User = require('../models/User');
+const { generateToken } = require('../config/auth');
 
-const registerUser = async (req, res) => {
-    const { name, email, password } = req.body;
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const user = new User({ name, email, password });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
+        const user = await User.findOne({ email });
+        if (!user || user.password !== password) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        const token = generateToken(user);
+        res.json({ token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
