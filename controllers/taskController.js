ee1b9bf9bbcb2b0710e26f22542113c646cc92a4
Bilal Azam <bilal.azam1@outlook.com>
2023-06-30 12:00:00 +0100

    Create Task assignment feature

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,57 +1,21 @@
 const Task = require('../models/Task');
+const User = require('../models/User');
 
-const createTask = async (req, res) => {
-    const { title, description, dueDate } = req.body;
+const assignTask = async (req, res) => {
+    const { taskId, userId } = req.body;
     try {
-        const task = new Task({ title, description, dueDate });
+        const task = await Task.findById(taskId);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: 'User not found' });
+
+        task.assignedTo = userId;
         await task.save();
-        res.status(201).json(task);
+        res.json({ message: 'Task assigned successfully', task });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
-const getTasks = async (req, res) => {
-    try {
-        const tasks = await Task.find();
-        res.json(tasks);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-const getTask = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const task = await Task.findById(id);
-        if (!task) return res.status(404).json({ message: 'Task not found' });
-        res.json(task);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-const updateTask = async (req, res) => {
-    const { id } = req.params;
-    const updates = req.body;
-    try {
-        const task = await Task.findByIdAndUpdate(id, updates, { new: true });
-        if (!task) return res.status(404).json({ message: 'Task not found' });
-        res.json(task);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-const deleteTask = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const task = await Task.findByIdAndDelete(id);
-        if (!task) return res.status(404).json({ message: 'Task not found' });
-        res.json({ message: 'Task deleted' });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-module.exports = { createTask, getTasks, getTask, updateTask, deleteTask };
+module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks, getTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask } = require('../controllers/taskController');
 
 router.post('/', createTask);
 router.get('/', getTasks);
 router.get('/:id', getTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
+router.post('/assign', assignTask); // New route for task assignment
 
 module.exports = router;
