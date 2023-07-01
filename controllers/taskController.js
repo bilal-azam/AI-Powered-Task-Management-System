6d558f6a92a912960e75aa25413d8d40dd078329
Bilal Azam <bilal.azam1@outlook.com>
2023-07-01 12:00:00 +0100

    Implement task filtering by user

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,21 +1,11 @@
-const Task = require('../models/Task');
-const User = require('../models/User');
-
-const assignTask = async (req, res) => {
-    const { taskId, userId } = req.body;
+const getTasksByUser = async (req, res) => {
+    const { userId } = req.query;
     try {
-        const task = await Task.findById(taskId);
-        if (!task) return res.status(404).json({ message: 'Task not found' });
-
-        const user = await User.findById(userId);
-        if (!user) return res.status(404).json({ message: 'User not found' });
-
-        task.assignedTo = userId;
-        await task.save();
-        res.json({ message: 'Task assigned successfully', task });
+        const tasks = await Task.find({ assignedTo: userId });
+        res.json(tasks);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
-module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask };
+module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, getTasksByUser };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask } = require('../controllers/taskController');
+const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, getTasksByUser } = require('../controllers/taskController');
 
 router.post('/', createTask);
 router.get('/', getTasks);
+router.get('/user', getTasksByUser); // New route for filtering tasks by user
 router.get('/:id', getTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
-router.post('/assign', assignTask); // New route for task assignment
+router.post('/assign', assignTask);
 
 module.exports = router;
