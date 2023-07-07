305a1de22044b9fd19655bbd888e85d9ef000cc5
Bilal Azam <bilal.azam1@outlook.com>
2023-07-07 12:00:00 +0100

    Add task completion status updates

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,11 +1,15 @@
-const getTasksByUser = async (req, res) => {
-    const { userId } = req.query;
+const updateTaskStatus = async (req, res) => {
+    const { taskId, status } = req.body;
     try {
-        const tasks = await Task.find({ assignedTo: userId });
-        res.json(tasks);
+        const task = await Task.findById(taskId);
+        if (!task) return res.status(404).json({ message: 'Task not found' });
+
+        task.status = status;
+        await task.save();
+        res.json(task);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
-module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, getTasksByUser };
+module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, updateTaskStatus };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, getTasksByUser } = require('../controllers/taskController');
+const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, updateTaskStatus } = require('../controllers/taskController');
 
 router.post('/', createTask);
 router.get('/', getTasks);
-router.get('/user', getTasksByUser); // New route for filtering tasks by user
 router.get('/:id', getTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
 router.post('/assign', assignTask);
+router.put('/status', updateTaskStatus); // New route for status updates
 
 module.exports = router;
