08f8bb3ab32f092b8d5733f5be02a4f9ac96fe3d
Bilal Azam <bilal.azam1@outlook.com>
2023-07-17 12:00:00 +0100

    Refactor and clean up code

--- config/database.js
+++ config/database.js
@@ -2,13 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ai-task-manager', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected successfully.');
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB connected');
     } catch (error) {
-        console.error('Failed to connect to MongoDB:', error);
+        console.error('Database connection error:', error.message);
         process.exit(1);
     }
 };
