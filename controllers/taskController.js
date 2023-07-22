c4cd93adfa587a822b16e121e38204938e475c39
Bilal Azam <bilal.azam1@outlook.com>
2023-07-22 12:00:00 +0100

    Add search functionality for tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,11 +1,13 @@
+const Task = require('../models/Task');
+
 const searchTasks = async (req, res) => {
     const { query } = req.query;
     try {
-        const tasks = await Task.find({ title: new RegExp(query, 'i') });
+        const tasks = await Task.find({ title: { $regex: query, $options: 'i' } });
         res.json(tasks);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 };
 
-module.exports = { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, updateTaskStatus, searchTasks };
+module.exports = { searchTasks };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { createTask, getTasks, getTask, updateTask, deleteTask, assignTask, updateTaskStatus, searchTasks } = require('../controllers/taskController');
+const { searchTasks } = require('../controllers/taskController');
 
-router.post('/', createTask);
-router.get('/', getTasks);
-router.get('/:id', getTask);
-router.put('/:id', updateTask);
-router.delete('/:id', deleteTask);
-router.post('/assign', assignTask);
-router.put('/status', updateTaskStatus);
-router.get('/search', searchTasks); // New route for search functionality
+router.get('/search', searchTasks);
 
 module.exports = router;
