cc80f240131f3d1fac0a218f0e812d94d6305ac4
Bilal Azam <bilal.azam1@outlook.com>
2023-07-31 12:00:00 +0100

    Conduct a final code review and optimization

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const i18n = require('./services/i18nService');
+const express = require('express');
+const app = express();
 
-// Initialize i18n
-app.use(i18n.init);
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const recommendationRoutes = require('./routes/recommendationRoutes');
+
+// Middleware
+app.use(express.json());
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+app.use('/admin', adminRoutes);
+app.use('/recommendations', recommendationRoutes);
+
+// Final code optimization
+app.use((req, res, next) => {
+    // Placeholder for final code optimization
+    next();
+});
+
+module.exports = app;
