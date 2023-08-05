72f7dc1f87dfe52734a5db80f37b1e62b762ac82
Bilal Azam <bilal.azam1@outlook.com>
2023-08-06 00:00:00 +0100

    Refactor product page to include recommended products section

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,9 +1,16 @@
-// Integrate Recommendation Engine
+// Refactor Product Page to Include Recommendations
 const { getRecommendations } = require('../features/recommendationEngine');
+const { RecommendationUI } = require('../components/RecommendationUI');
 
 function renderProductPage(productId) {
     const recommendations = getRecommendations(productId);
     // Render product details and recommendations
+    return (
+        <div>
+            <h1>Product Details</h1>
+            <RecommendationUI recommendations={recommendations} />
+        </div>
+    );
 }
 
 module.exports = { renderProductPage };
