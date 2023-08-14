cdff5c7678471c3cdd4367e7964f00a8b99d4de1
Bilal Azam <bilal.azam1@outlook.com>
2023-08-15 00:00:00 +0100

    Refactor search page to enhance performance

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -1,9 +1,16 @@
-// Integrate AI-based Search with Search Page
+// Refactor Search Page to Enhance Performance
 const { searchProducts } = require('../features/productSearch');
+const { SearchResultsUI } = require('../components/SearchResultsUI');
 
 function renderSearchPage(query) {
     const results = searchProducts(query);
     // Render search results
+    return (
+        <div>
+            <h1>Search</h1>
+            <SearchResultsUI results={results} />
+        </div>
+    );
 }
 
 module.exports = { renderSearchPage };
