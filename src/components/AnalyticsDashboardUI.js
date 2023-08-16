80bbc97d5424657ed6abcf478ecf8551fdff186a
Bilal Azam <bilal.azam1@outlook.com>
2023-08-17 00:00:00 +0100

    Add UI for analytics dashboard

---
+++ src/components/AnalyticsDashboardUI.js
@@ -0,0 +1,15 @@
+// UI Component for Analytics Dashboard
+function AnalyticsDashboardUI({ data }) {
+    return (
+        <div className="analytics-dashboard-ui">
+            <h2>Analytics Dashboard</h2>
+            <div>
+                <p>Total Sales: {data.totalSales}</p>
+                <p>Total Users: {data.totalUsers}</p>
+                <p>Total Orders: {data.totalOrders}</p>
+            </div>
+        </div>
+    );
+}
+
+module.exports = { AnalyticsDashboardUI };
