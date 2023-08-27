09f6fc4b44480278259ba2c2a00da94b2ba69230
Bilal Azam <bilal.azam1@outlook.com>
2023-08-28 00:00:00 +0100

    Integrate order history with user profile page

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -1,6 +1,9 @@
-// Feature for Viewing Purchase History
-function renderPurchaseHistory(userId) {
-    // Fetch and display user's purchase history
+// Integrate Order History with User Profile
+const { getOrderHistory } = require('../features/orderHistory');
+
+function renderUserProfile(userId) {
+    const orderHistory = getOrderHistory(userId);
+    // Render user profile and order history
 }
 
-module.exports = { renderPurchaseHistory };
+module.exports = { renderUserProfile };
