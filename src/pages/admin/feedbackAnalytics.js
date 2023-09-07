3cdf8b13584d77bb236b82cdcc588134f80d1408
Bilal Azam <bilal.azam1@outlook.com>
2023-09-08 00:00:00 +0100

    Add analytics for feedback sentiment

---
+++ src/pages/admin/feedbackAnalytics.js
@@ -0,0 +1,6 @@
+// Feedback Sentiment Analytics for Admins
+function renderFeedbackAnalytics() {
+    // Fetch and display feedback sentiment data
+}
+
+module.exports = { renderFeedbackAnalytics };
