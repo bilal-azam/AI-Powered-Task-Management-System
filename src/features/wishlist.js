04c0895bf0c4237f32c88f255f74fd6f94a37147
Bilal Azam <bilal.azam1@outlook.com>
2023-09-13 00:00:00 +0100

    Implement wishlist feature for users

---
+++ src/features/wishlist.js
@@ -0,0 +1,12 @@
+// Wishlist Feature for Users
+function addToWishlist(userId, productId) {
+    // Logic to add a product to user's wishlist
+    return true;
+}
+
+function getWishlist(userId) {
+    // Logic to retrieve user's wishlist
+    return [];
+}
+
+module.exports = { addToWishlist, getWishlist };
