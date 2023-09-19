f1ba526eed731275b9f7f7858661a74adf65f56b
Bilal Azam <bilal.azam1@outlook.com>
2023-09-20 00:00:00 +0100

    Add UI for account deactivation process

---
+++ src/components/AccountDeactivationUI.js
@@ -0,0 +1,11 @@
+// UI Component for Account Deactivation Process
+function AccountDeactivationUI() {
+    return (
+        <div className="account-deactivation-ui">
+            <h2>Deactivate Your Account</h2>
+            <button>Deactivate Account</button>
+        </div>
+    );
+}
+
+module.exports = { AccountDeactivationUI };
