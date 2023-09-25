b056cf91735ee9ccaa153ffbbb64b12bfa6589be
Bilal Azam <bilal.azam1@outlook.com>
2023-09-26 00:00:00 +0100

    Add UI for account reactivation process

---
+++ src/components/AccountReactivationUI.js
@@ -0,0 +1,11 @@
+// UI Component for Account Reactivation Process
+function AccountReactivationUI() {
+    return (
+        <div className="account-reactivation-ui">
+            <h2>Reactivate Your Account</h2>
+            <button>Reactivate Account</button>
+        </div>
+    );
+}
+
+module.exports = { AccountReactivationUI };
