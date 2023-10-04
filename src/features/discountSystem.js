dc109b1318e5ab8baf74b502d8004f24e4ef16a8
Bilal Azam <bilal.azam1@outlook.com>
2023-10-05 00:00:00 +0100

    Implement discount and coupon system

---
+++ src/features/discountSystem.js
@@ -0,0 +1,7 @@
+// Discount and Coupon System
+function applyDiscount(userId, couponCode) {
+    // Logic to apply discount based on coupon code
+    return { success: true, discountAmount: 10 };
+}
+
+module.exports = { applyDiscount };
