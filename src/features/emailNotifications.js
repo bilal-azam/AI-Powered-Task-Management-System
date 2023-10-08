f5f2f4be8cee2f00f079758db620a0341af1d7cb
Bilal Azam <bilal.azam1@outlook.com>
2023-10-09 00:00:00 +0100

    Implement email notifications for order updates

---
+++ src/features/emailNotifications.js
@@ -0,0 +1,7 @@
+// Email Notifications for Order Updates
+function sendOrderUpdateEmail(orderId, userId, status) {
+    // Logic to send email notification
+    return true;
+}
+
+module.exports = { sendOrderUpdateEmail };
