595933f42b5d0d251f2a2fe9311d10d063ec64d5
Bilal Azam <bilal.azam1@outlook.com>
2023-10-13 00:00:00 +0100

    Implement search functionality with filters

---
+++ src/features/search.js
@@ -0,0 +1,7 @@
+// Search Functionality with Filters
+function searchProducts(query, filters) {
+    // Logic to search products based on query and filters
+    return [];
+}
+
+module.exports = { searchProducts };
