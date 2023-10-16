5e0af19d8934626985a78da99f930ee98cd09e40
Bilal Azam <bilal.azam1@outlook.com>
2023-10-17 00:00:00 +0100

    Implement multi-language support

---
+++ src/features/languageSupport.js
@@ -0,0 +1,7 @@
+// Multi-Language Support
+function translateText(text, language) {
+    // Logic to translate text to the specified language
+    return text;
+}
+
+module.exports = { translateText };
