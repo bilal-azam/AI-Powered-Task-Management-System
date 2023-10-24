9a216c07819e32000dd6e57a3ebc7b7ead7c2c7d
Bilal Azam <bilal.azam1@outlook.com>
2023-10-25 00:00:00 +0100

    Implement user roles and permissions

---
+++ src/features/userRoles.js
@@ -0,0 +1,12 @@
+// User Roles and Permissions System
+function assignRole(userId, role) {
+    // Logic to assign a role to a user
+    return true;
+}
+
+function checkPermission(userId, action) {
+    // Logic to check if the user has permission for a specific action
+    return true;
+}
+
+module.exports = { assignRole, checkPermission };
