52c7e73a84e20bebfc1eaf56ecf5e7018779d48a
Bilal Azam <bilal.azam1@outlook.com>
2023-11-06 00:00:00 +0000

    Add UI for wishlist feature

--- src/components/WishlistUI.js
+++ src/components/WishlistUI.js
@@ -1,11 +1,16 @@
-// Refactor Wishlist UI for Better User Experience
-function WishlistUI({ wishlist }) {
+// UI Component for Wishlist Feature
+function WishlistUI({ userId, wishlist, onAddToWishlist }) {
     return (
         <div className="wishlist-ui">
-            <h2>Your Wishlist</h2>
+            <h2>My Wishlist</h2>
             <ul>
-                {wishlist.map(product => (
-                    <li key={product.id}>{product.name}</li>
+                {wishlist.map((product, index) => (
+                    <li key={index}>
+                        <p>{product.name}</p>
+                        <button onClick={() => onAddToWishlist(userId, product.id)}>
+                            Add to Wishlist
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
