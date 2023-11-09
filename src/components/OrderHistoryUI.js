2e1be54103779ab6f74dbb5e40ce1f743eaae68b
Bilal Azam <bilal.azam1@outlook.com>
2023-11-09 00:00:00 +0000

    Add UI for order history feature

--- src/components/OrderHistoryUI.js
+++ src/components/OrderHistoryUI.js
@@ -1,11 +1,15 @@
-// UI Component for Viewing Order History
-function OrderHistoryUI({ history }) {
+// UI Component for Order History Feature
+function OrderHistoryUI({ userId, orders }) {
     return (
         <div className="order-history-ui">
-            <h2>Order History</h2>
+            <h2>My Order History</h2>
             <ul>
-                {history.map(order => (
-                    <li key={order.id}>{order.productName}</li>
+                {orders.map((order, index) => (
+                    <li key={index}>
+                        <p>Order #{order.id}</p>
+                        <p>Date: {order.date}</p>
+                        <p>Total: {order.total}</p>
+                    </li>
                 ))}
             </ul>
         </div>
