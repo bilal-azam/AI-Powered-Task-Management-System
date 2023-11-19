47f426c11107cc07d06a2276e7d4af9936e17ffb
Bilal Azam <bilal.azam1@outlook.com>
2023-11-19 00:00:00 +0000

    Test AI-based recommendation system

--- tests/recommendationSystem.test.js
+++ tests/recommendationSystem.test.js
@@ -1,7 +1,7 @@
-// Test cases for Product Recommendation System
+// Test cases for AI-based Recommendation System
 const { recommendProducts } = require('../src/features/recommendationSystem');
 
 test('should recommend products based on user behavior', () => {
     const recommendations = recommendProducts('user123');
-    expect(recommendations).toBeInstanceOf(Array);
+    expect(Array.isArray(recommendations)).toBe(true);
 });
