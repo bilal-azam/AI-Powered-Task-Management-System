34ac57efe091b11e7209ff71381a0f862496522c
Bilal Azam <bilal.azam1@outlook.com>
2023-11-25 00:00:00 +0000

    Test user feedback system

--- tests/feedbackSystem.test.js
+++ tests/feedbackSystem.test.js
@@ -1,7 +1,12 @@
-// Test cases for Feedback System
-const { collectFeedback } = require('../src/features/feedbackSystem');
+// Test cases for User Feedback System
+const { submitFeedback, getUserFeedback } = require('../src/features/feedbackSystem');
 
-test('should collect user feedback', () => {
-    const result = collectFeedback('user123', 'Great product!');
+test('should submit user feedback', () => {
+    const result = submitFeedback('user123', 'Great experience!');
     expect(result).toBe(true);
+});
+
+test('should retrieve user feedback', () => {
+    const feedback = getUserFeedback('user123');
+    expect(Array.isArray(feedback)).toBe(true);
 });
