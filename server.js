69431de8a8d43ee6a6a7b5ca40636490524ef7e8
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:32:14 +0100

    feat: Set up performance monitoring with New Relic

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require('./newrelic');
+
 const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
