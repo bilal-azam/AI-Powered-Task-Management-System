c6a441859cc9053b3b12a92b3b97fbaa5f8d6fbc
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:52 +0100

    feat: Add login page

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
+
+export default LoginPage;
