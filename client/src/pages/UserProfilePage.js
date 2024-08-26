81235b3fd08bee017680ef74684daaed721d4ffd
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:50 +0100

    feat: Integrate user profile with frontend

---
+++ client/src/pages/UserProfilePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserProfile from '../components/UserProfile';
+
+const UserProfilePage = () => {
+    return (
+        <div>
+            <h1>User Profile</h1>
+            <UserProfile />
+        </div>
+    );
+};
+
+export default UserProfilePage;
