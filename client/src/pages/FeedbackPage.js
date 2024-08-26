034376ad42018df5a104b9d2b50120dbc46386b1
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:47 +0100

    feat: Integrate feedback form with frontend

---
+++ client/src/pages/FeedbackPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FeedbackForm from '../components/FeedbackForm';
+
+const FeedbackPage = () => {
+    return (
+        <div>
+            <h1>Feedback</h1>
+            <FeedbackForm />
+        </div>
+    );
+};
+
+export default FeedbackPage;
