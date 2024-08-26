3e7d02ccd00e48e05917c57960bc41136e7d60b6
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:53 +0100

    feat: Add logout button to user profile page

--- client/src/pages/UserProfilePage.js
+++ client/src/pages/UserProfilePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import UserProfile from '../components/UserProfile';
+import LogoutButton from '../components/LogoutButton';
 
 const UserProfilePage = () => {
     return (
         <div>
             <h1>User Profile</h1>
             <UserProfile />
+            <LogoutButton />
         </div>
     );
 };
