ffea063ab4f98c6aa728768e0bf9100527bfb448
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:26:17 +0100

    feat: Add a REST API documentation page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const limiter = require('./middlewares/rateLimitMiddleware');
+const path = require('path');
 
 const app = express();
-app.use(limiter);
+
+app.use('/docs', express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
