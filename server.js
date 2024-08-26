d80cfa94136d30baab55f58f195295d31714ab5b
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:26:17 +0100

    feat: Implement rate limiting

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const i18n = require('./services/i18nService');
+const limiter = require('./middlewares/rateLimitMiddleware');
 
 const app = express();
-app.use(i18n.init);
+app.use(limiter);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
