7bfd69b8608370f72c3c17b044ee988eb106f551
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:51 +0100

    feat: Add user profile page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AnalyticsPage from './pages/AnalyticsPage';
-import FeedbackPage from './pages/FeedbackPage';
+import UserProfilePage from './pages/UserProfilePage';
 
 const App = () => {
     return (
         <Router>
             <Switch>
+                <Route path="/user-profile" component={UserProfilePage} />
                 <Route path="/analytics" component={AnalyticsPage} />
                 <Route path="/feedback" component={FeedbackPage} />
                 <Route path="/" component={HomePage} />
