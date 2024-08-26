b5d35d530b78992b75b3c22d0a7848627f87d57b
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:37:46 +0100

    feat: Create backend endpoint for feedback submission

--- controllers/feedbackController.js
+++ controllers/feedbackController.js
@@ -1,14 +1,7 @@
-const Feedback = require('../models/Feedback');
-
 const submitFeedback = async (req, res) => {
-    try {
-        const { userId, message } = req.body;
-        const feedback = new Feedback({ userId, message });
-        await feedback.save();
-        res.status(201).send('Feedback submitted');
-    } catch (error) {
-        res.status(500).send('Error submitting feedback');
-    }
+    // Save feedback to the database
+    console.log('Feedback received:', req.body.feedback);
+    res.status(200).send('Feedback received');
 };
 
 module.exports = { submitFeedback };
