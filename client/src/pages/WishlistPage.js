43a4630823fc2f1f19310fd5c1e322ba0edd6ae9
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:43:18 +0100

    feat: Integrate wishlist feature with frontend

---
+++ client/src/pages/WishlistPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wishlist from '../components/Wishlist';
+
+const WishlistPage = () => {
+    return (
+        <div>
+            <h1>Wishlist</h1>
+            <Wishlist />
+        </div>
+    );
+};
+
+export default WishlistPage;
