1995a0617a900c4f708c4b5e9af9e13a68ff87c8
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:43:21 +0100

    feat: Integrate notifications feature with frontend

---
+++ client/src/pages/NotificationsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Notifications from '../components/Notifications';
+
+const NotificationsPage = () => {
+    return (
+        <div>
+            <h1>Notifications</h1>
+            <Notifications />
+        </div>
+    );
+};
+
+export default NotificationsPage;
