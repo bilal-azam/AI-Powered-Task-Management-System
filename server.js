4f577bfacb6515332c758b0e93aade5c0ee9f6ce
Bilal Azam <bilal.azam1@outlook.com>
2024-08-26 02:26:17 +0100

    feat: Add support for multiple languages (i18n)

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const loggerMiddleware = require('./middlewares/loggerMiddleware');
+const i18n = require('./services/i18nService');
 
 const app = express();
-app.use(loggerMiddleware);
+app.use(i18n.init);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
--- services/i18nService.js
+++ services/i18nService.js
@@ -1,12 +1,13 @@
 const i18n = require('i18n');
+const path = require('path');
 
 i18n.configure({
     locales: ['en', 'es'],
-    directory: __dirname + '/../locales',
+    directory: path.join(__dirname, '../i18n'),
     defaultLocale: 'en',
     autoReload: true,
     syncFiles: true,
-    updateFiles: false,
+    queryParameter: 'lang'
 });
 
 module.exports = i18n;
